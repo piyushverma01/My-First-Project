70d81352bc871912635f4b0246a1e230a6fc7c8a
piyushverma01 <111368157+piyushverma01@users.noreply.github.com>
2022-08-25 20:33:50 +0530

    let the engine run

--- TRAIN.C
+++ TRAIN.C
@@ -1,58 +1,59 @@
 #include<stdio.h>
 #include<graphics.h>
 #include<conio.h>
+#include<dos.h>
 void main()
 {
-
+int i;
 int gdriver= DETECT,gm,errorcode;
 initgraph(&gdriver,&gm,"c:\\TURBOC3\\bgi");
-//for(i=230;i<650;i++)
-//{
+for(i=0;i<getmaxx();i++)
+{
 setbkcolor(LIGHTBLUE);
 setcolor(WHITE);
 setfillstyle(SOLID_FILL,WHITE);
 
    //FOR CLOUD 1
 
-line(330,100,422,100);
+line(330+i,100,422+i,100);
 
 //upper side
 
-sector(345,99,90,180,15,10);
-sector(360,99,90,180,20,15);
-sector(375,99,0,180,25,20);
-sector(390,99,0,180,20,25);
-sector(415,100,0,180,15,10);
+sector(345+i,99,90,180,15,10);
+sector(360+i,99,90,180,20,15);
+sector(375+i,99,0,180,25,20);
+sector(390+i,99,0,180,20,25);
+sector(415+i,100,0,180,15,10);
 
 //lower side
 
 
-sector(345,99,180,360,15,10);
-sector(360,99,180,360,20,15);
-sector(375,99,180,360,25,20);
-sector(390,99,180,360,20,25);
-sector(415,100,180,360,15,10);
+sector(345+i,99,180,360,15,10);
+sector(360+i,99,180,360,20,15);
+sector(375+i,99,180,360,25,20);
+sector(390+i,99,180,360,20,25);
+sector(415+i,100,180,360,15,10);
 
 //cloud 2
 
-   line(100,150,190,150);
+   line(100+i,150,190+i,150);
 
 //upper side
 
-sector(115,150,90,180,15,10);
-sector(130,150,90,180,20,15);
-sector(145,150,0,180,25,20);
-sector(160,150,0,180,20,25);
-sector(175,150,0,180,15,10);
+sector(115+i,150,90,180,15,10);
+sector(130+i,150,90,180,20,15);
+sector(145+i,150,0,180,25,20);
+sector(160+i,150,0,180,20,25);
+sector(175+i,150,0,180,15,10);
 
 //lower side
 
 
-sector(115,150,180,360,15,10);
-sector(130,150,180,360,20,15);
-sector(145,150,180,360,25,20);
-sector(160,150,180,360,20,25);
-sector(175,150,180,360,15,10);
+sector(115+i,150,180,360,15,10);
+sector(130+i,150,180,360,20,15);
+sector(145+i,150,180,360,25,20);
+sector(160+i,150,180,360,20,25);
+sector(175+i,150,180,360,15,10);
 
 //BRIDGE
 
@@ -126,121 +127,123 @@ lineto(getmaxx()-17,getmaxy());
 
 setcolor(RED);
 setfillstyle(SOLID_FILL,RED);
-rectangle(0,getmaxy()-235,25,getmaxy()-220);
-line(0,getmaxy()-228,25,getmaxy()-228);
-floodfill(10,getmaxy()-225,RED);
+rectangle(0+i,getmaxy()-235,25+i,getmaxy()-220);
+line(0+i,getmaxy()-228,25+i,getmaxy()-228);
+floodfill(10+i,getmaxy()-225,RED);
 
 //wheels of last boogie
 
 setcolor(BLUE);
 setfillstyle(SOLID_FILL,BLUE);
-pieslice(6,getmaxy()-220,0,360,4);
-pieslice(18,getmaxy()-220,0,360,4);
+pieslice(6+i,getmaxy()-220,0,360,4);
+pieslice(18+i,getmaxy()-220,0,360,4);
 
 // lines joining last and second last boogie
 
-line(26,getmaxy()-228,32,getmaxy()-228);
-line(26,getmaxy()-225,32,getmaxy()-225);
+line(26+i,getmaxy()-228,32+i,getmaxy()-228);
+line(26+i,getmaxy()-225,32+i,getmaxy()-225);
 
 //second last boogie
 
 setcolor(RED);
 setfillstyle(SOLID_FILL,RED);
-rectangle(32,getmaxy()-235,70,getmaxy()-220);
-line(32,getmaxy()-230,70,getmaxy()-230);
-floodfill(35,getmaxy()-225,RED);
+rectangle(32+i,getmaxy()-235,70+i,getmaxy()-220);
+line(32+i,getmaxy()-230,70+i,getmaxy()-230);
+floodfill(35+i,getmaxy()-225,RED);
 
 //wheels of second last boogie
 
 setcolor(BLUE);
 setfillstyle(SOLID_FILL,BLUE);
-pieslice(40,getmaxy()-220,0,360,4);
-pieslice(60,getmaxy()-220,0,360,4);
+pieslice(40+i,getmaxy()-220,0,360,4);
+pieslice(60+i,getmaxy()-220,0,360,4);
 
 //line joining second last and preciding
 
-line(71,getmaxy()-228,77,getmaxy()-228);
-line(71,getmaxy()-225,77,getmaxy()-225);
+line(71+i,getmaxy()-228,77+i,getmaxy()-228);
+line(71+i,getmaxy()-225,77+i,getmaxy()-225);
 
 //third last boogie
 
 setcolor(RED);
 setfillstyle(SOLID_FILL,RED);
-rectangle(77,getmaxy()-235,115,getmaxy()-220);
-line(77,getmaxy()-230,115,getmaxy()-230);
-floodfill(78,getmaxy()-228,RED);
+rectangle(77+i,getmaxy()-235,115+i,getmaxy()-220);
+line(77+i,getmaxy()-230,115+i,getmaxy()-230);
+floodfill(78+i,getmaxy()-228,RED);
 
 //wheels of third last boogie
 
 setcolor(BLUE);
 setfillstyle(SOLID_FILL,BLUE);
-pieslice(85,getmaxy()-220,0,360,4);
-pieslice(105,getmaxy()-220,0,360,4);
+pieslice(85+i,getmaxy()-220,0,360,4);
+pieslice(105+i,getmaxy()-220,0,360,4);
 
 //line joining third and preciding boogie
 
-line(116,getmaxy()-228,122,getmaxy()-228);
-line(116,getmaxy()-225,122,getmaxy()-225);
+line(116+i,getmaxy()-228,122+i,getmaxy()-228);
+line(116+i,getmaxy()-225,122+i,getmaxy()-225);
 
 //fourth last boogie
 
 setcolor(RED);
 setfillstyle(SOLID_FILL,RED);
-rectangle(122,getmaxy()-235,160,getmaxy()-220);
-line(122,getmaxy()-230,160,getmaxy()-230);
-floodfill(124,getmaxy()-228,RED);
+rectangle(122+i,getmaxy()-235,160+i,getmaxy()-220);
+line(122+i,getmaxy()-230,160+i,getmaxy()-230);
+floodfill(124+i,getmaxy()-228,RED);
 
 //wheels for third last boogie
 
 setcolor(BLUE);
 setfillstyle(SOLID_FILL,BLUE);
-pieslice(130,getmaxy()-220,0,360,4);
-pieslice(150,getmaxy()-220,0,360,4);
+pieslice(130+i,getmaxy()-220,0,360,4);
+pieslice(150+i,getmaxy()-220,0,360,4);
 
 //line joining third last and engine
 
-line(161,getmaxy()-228,167,getmaxy()-228);
-line(161,getmaxy()-225,167,getmaxy()-225);
+line(161+i,getmaxy()-228,167+i,getmaxy()-228);
+line(161+i,getmaxy()-225,167+i,getmaxy()-225);
 
 //engine of the train
 
 setcolor(RED);
 setfillstyle(SOLID_FILL,RED);
-rectangle(167,getmaxy()-245,185,getmaxy()-220);
+rectangle(167+i,getmaxy()-245,185+i,getmaxy()-220);
 
-line(167,getmaxy()-240,185,getmaxy()-240);
-floodfill(169,getmaxy()-238,RED);
+line(167+i,getmaxy()-240,185+i,getmaxy()-240);
+floodfill(169+i,getmaxy()-238,RED);
 
-rectangle(185,getmaxy()-235,205,getmaxy()-220);
-floodfill(186,getmaxy()-232,RED);
+rectangle(185+i,getmaxy()-235,205+i,getmaxy()-220);
+floodfill(186+i,getmaxy()-232,RED);
 
-rectangle(190,getmaxy()-250,196,getmaxy()-235);
-floodfill(191,getmaxy()-249,RED);
+rectangle(190+i,getmaxy()-250,196+i,getmaxy()-235);
+floodfill(191+i,getmaxy()-249,RED);
 
 //smoke of engine
 
 setcolor(7);
-line(190,getmaxy()-253,196,getmaxy()-253);
-line(190,getmaxy()-255,192,getmaxy()-255);
-line(194,getmaxy()-255,196,getmaxy()-255);
-line(188,getmaxy()-257,194,getmaxy()-257);
-line(187,getmaxy()-259,193,getmaxy()-259);
-line(185,getmaxy()-261,190,getmaxy()-261);
-line(192,getmaxy()-261,194,getmaxy()-261);
-line(188,getmaxy()-263,192,getmaxy()-263);
-line(180,getmaxy()-265,190,getmaxy()-265);
+line(190+i,getmaxy()-253,196+i,getmaxy()-253);
+line(190+i,getmaxy()-255,192+i,getmaxy()-255);
+line(194+i,getmaxy()-255,196+i,getmaxy()-255);
+line(188+i,getmaxy()-257,194+i,getmaxy()-257);
+line(187+i,getmaxy()-259,193+i,getmaxy()-259);
+line(185+i,getmaxy()-261,190+i,getmaxy()-261);
+line(192+i,getmaxy()-261,194+i,getmaxy()-261);
+line(188+i,getmaxy()-263,192+i,getmaxy()-263);
+line(180+i,getmaxy()-265,190+i,getmaxy()-265);
 
 //wheels of engine
 setcolor(BLUE);
 setfillstyle(SOLID_FILL,BLUE);
-pieslice(175,getmaxy()-220,0,360,5);
-pieslice(188,getmaxy()-220,0,360,4);
-pieslice(200,getmaxy()-220,0,360,4);
+pieslice(175+i,getmaxy()-220,0,360,5);
+pieslice(188+i,getmaxy()-220,0,360,4);
+pieslice(200+i,getmaxy()-220,0,360,4);
 
 //front section of engine
 
-sector(206,getmaxy()-228,0,90,8,7);
-sector(206,getmaxy()-228,270,360,8,8);
-
+sector(206+i,getmaxy()-228,0,90,8,7);
+sector(206+i,getmaxy()-228,270,360,8,8);
+delay(150);
+cleardevice();
+}
 getch();
 }
